9c8df55e460c58399b09948103bfb330aaddd164
reneboygarcia <reneboygarcia@mgmail.com>
2023-02-10 16:46:20 +0800

    update code

--- etl_gcs_to_bq_wk3.py
+++ etl_gcs_to_bq_wk3.py
@@ -9,31 +9,28 @@ from prefect_gcp import GcpCredentials
 print("Setup Complete")
 
 # Download trip data from GCS
-@task(log_prints=True)
-def extract_from_gcs(color: str, year: int, month: int) -> Path:
-    gcs_path = f"data/{color}/{color}_tripdata_{year}-{month:02}.parquet"
+@task(log_prints=True, name="extract-from-gcs")
+def extract_from_gcs(year: int, month: int) -> Path:
+    gcs_path = f"fhv_tripdata_{year}-{month:02}.parquet"
     gcs_block = GcsBucket.load("prefect-gcs-2023")
-    gcs_block.get_directory(
-        from_path=gcs_path, local_path="./data/"
-    )  # the .. indicates the folder level
-    return Path(f"./data/{gcs_path}")
+    gcs_block.get_directory(from_path=gcs_path, local_path=f"{year}/")
+    return Path(f"{year}/{gcs_path}")
 
 
 # Data cleaning example
-@task(log_prints=True)
+@task(log_prints=True, name="transform")
 def transform(path: Path) -> pd.DataFrame:
-    df = pd.read_parquet(path)
-    print(f"Pre: Missing passenger count: {df.passenger_count.isna().sum()}")
-    df.fillna(value={"passenger_count": 0}, inplace=True)
-    print(f"Post: Missing passenger count: {df.passenger_count.isna().sum()}")
+    df = pd.read_parquet(path, engine="pyarrow")
+    print(f"Number of rows: {df.shape[0]}")
+    print(f"Columns Dtype: {df.dtypes}")
     return df
 
 
 # Write DataFrame to BigQuery
-def write_bq(df: pd.DataFrame) -> None:
-    gcp_credentials_block = GcpCredentials.load("prefect-gcs-2023-creds")
+def write_bq(df: pd.DataFrame, year: int) -> None:
+    gcp_credentials_block = GcpCredentials.load("ny-taxi-gcp-creds")
     df.to_gbq(
-        destination_table="prefect_de_2023_datasetid.yellow_ny_taxi_trips_table",
+        destination_table=f"ny_taxi.ny_taxi_tripdata_{year}",
         project_id="dtc-de-2023",
         credentials=gcp_credentials_block.get_credentials_from_service_account(),
         chunksize=500_000,
@@ -41,18 +38,33 @@ def write_bq(df: pd.DataFrame) -> None:
     )
 
 
-# Main ETL flow to load data to BigQuery
-@flow(log_prints=True)
+# ETL flow to load data to BigQuery
+@flow(log_prints=True, name="etl-gcs-to-bq")
 def etl_gcs_to_bq():
-    color = "yellow"
-    year = 2021
+    year = 2019
     month = 1
 
     # step-by-step execution
-    path = extract_from_gcs(color, year, month)
+    path = extract_from_gcs(year, month)
     df = transform(path)
-    write_bq(df)
+    write_bq(df, year)
 
+
+# Parent flow ETL
+@flow(log_prints=True, name="etl-parent-to-bq")
+def etl_parent_bq_flow(
+    year: int = 2019, months: list[int] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+):
+    for month in months:
+        etl_gcs_to_bq(year, month)
+
+
+# run main
+if __name__ == "__main__":
+    year = 2019
+    months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+    etl_parent_bq_flow(year, months)
 
 # run main
 if __name__ == "__main__":
