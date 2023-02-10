f9eee55f6f667ed993c5261f571af3561f5130f4
reneboygarcia <reneboygarcia@mgmail.com>
2023-02-10 23:31:29 +0800

    remove some schema

--- etl_gcs_to_bq_wk3.py
+++ etl_gcs_to_bq_wk3.py
@@ -23,8 +23,6 @@ def etl_gcs_to_bq(year: int, month: int):
             bigquery.SchemaField("dispatching_base_num", "STRING", mode="NULLABLE"),
             bigquery.SchemaField("pickup_datetime", "TIMESTAMP", mode="NULLABLE"),
             bigquery.SchemaField("dropOff_datetime", "TIMESTAMP", mode="NULLABLE"),
-            bigquery.SchemaField("PUlocationID", "INT64", mode="NULLABLE"),
-            bigquery.SchemaField("DOlocationID", "INT64", mode="NULLABLE"),
             bigquery.SchemaField("Affiliated_base_number", "STRING", mode="NULLABLE"),
         ],
     )
